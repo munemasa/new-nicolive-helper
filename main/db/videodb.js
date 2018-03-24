cb687f747088b1ae608039003c70a4e58a8515d0
amano <amano@miku39.jp>
2018-03-24 18:32:09 +0900

    ??DB????

--- main/db/videodb.js
+++ main/db/videodb.js
@@ -38,14 +38,6 @@ var VideoDB = {
         db.videodb.put( vinfo );
     },
 
-    test: async function(){
-        let d = await this.db.videodb.get( 'sm9' );
-        console.log( d );
-
-        let r = await this.db.videodb.where( 'view_counter' ).above( 10000 ).toArray();
-        console.log( r );
-    },
-
     /**
      * 検索条件を追加
      */
@@ -65,10 +57,162 @@ var VideoDB = {
         RemoveElement( ev.target.parentNode );
     },
 
-    init: function(){
+    addVideos: async function(){
+        let str = $( '#input-video' ).val();
+        if( str.length < 3 ) return;
+
+
+        $( '#information' ).text( '動画を追加/更新しています...' );
+        let l = str.match( /(sm|nm|so)\d+|\d{10}/g );
+
+        for( let i = 0, id; id = l[i]; i++ ){
+            try{
+                let vinfo = await window.opener.NicoLiveHelper.getVideoInfo( id );
+                this.db.videodb.put( vinfo );
+                console.log( `${id}を追加しました` );
+            }catch( e ){
+            }
+        }
+        $( '#information' ).text( 'DB追加/更新完了しました' );
+        $( '#input-video' ).val( '' );
+    },
+
+    test: async function(){
+        let d = await this.db.videodb.get( 'sm9' );
+        console.log( d );
+
+        let r = await this.db.videodb.where( 'view_counter' ).above( 10000 ).toArray();
+        console.log( r );
+    },
+
+    createListElement: function( item ){
+        let t = document.querySelector( '#template-video-info' );
+        let clone2 = document.importNode( t.content, true );
+        let elem = clone2.firstElementChild;
+        let image = elem.querySelector( '.video-thumbnail' );
+
+        let posteddate = GetDateString( item.first_retrieve * 1000, true );
+
+        if( item.deleted != 0 ){
+            $( elem ).addClass( "video-deleted" );
+        }
+        image.setAttribute( 'src', item.thumbnail_url );
+        image.setAttribute( 'style', 'width:65px;height:50px;margin-right:8px;' );
+        let details = elem.querySelector( '.video-details' );
+
+        let min = parseInt( item.length_ms / 1000 / 60 );
+        let sec = parseInt( item.length_ms / 1000 % 60 );
+
+        $( elem.querySelector( '.video-title' ) ).text( `${item.video_id} ${item.title}` );
+        $( elem.querySelector( '.open-page' ) ).attr( 'href', `http://www.nicovideo.jp/watch/${item.video_id}` );
+
+        details.appendChild( document.createTextNode( "投稿:" + posteddate + " 時間:" + (min + ":" + (sec < 10 ? ("0" + sec) : sec)) ) );
+        details.appendChild( document.createElement( 'br' ) );
+        details.appendChild( document.createTextNode( "再生:" + FormatCommas( item.view_counter )
+            + " コメント:" + FormatCommas( item.comment_num )
+            + " マイリスト:" + FormatCommas( item.mylist_counter ) ) );
+
+        return elem;
+    },
+
+
+    searchVideos: async function(){
+        let types = $( '.search-type' );
+        let conds = $( '.search-cond' );
+        let vals = $( '.search-value' );
+        let n = types.length;
+
+        let filter_func = [];
+        for( let i = 0; i < n; i++ ){
+            let t = $( types[i] ).val();
+            let c = $( conds[i] ).val();
+            let v = $( vals[i] ).val();
+            if( t == 'length_ms' ){
+                v *= 1000;
+            }
+
+            switch( c ){
+            case 'include':
+                filter_func.push( ( item ) =>{
+                    let reg = new RegExp( v, 'i' );
+                    if( t == 'tags_array' ){
+                        for( let t of item.tags_array ){
+                            if( t.match( reg ) ){
+                                return true;
+                            }
+                        }
+                        return false;
+                    }else{
+                        return item[t].match( reg );
+                    }
+                } );
+                break;
+            case 'exclude':
+                filter_func.push( ( item ) =>{
+                    let reg = new RegExp( v, 'i' );
+                    if( t == 'tags_array' ){
+                        for( let t of item.tags_array ){
+                            if( t.match( reg ) ){
+                                return false;
+                            }
+                        }
+                        return true;
+                    }else{
+                        return !item[t].match( reg );
+                    }
+                } );
+                break;
+            case 'greater_than':
+                filter_func.push( ( item ) =>{
+                    return item[t] > v;
+                } );
+                break;
+            case 'less_than':
+                filter_func.push( ( item ) =>{
+                    return item[t] < v;
+                } );
+                break;
+            case 'equal':
+            default:
+                filter_func.push( ( item ) =>{
+                    return item[t] == v;
+                } );
+                break;
+            }
+        }
+        let result = await this.db.videodb.filter( ( item ) =>{
+            for( let f of filter_func ){
+                let flg = f( item );
+                if( !flg ) return false;
+            }
+            return true;
+        } ).toArray();
+
+        console.log( result );
+        $( '#information' ).text( `${result.length}件見つかりました。` );
+
+        $( '#tbl-result' ).empty();
+        for( let vinfo of result ){
+            let elem = this.createListElement( vinfo );
+            $( '#tbl-result' ).append( elem );
+        }
+        return result;
+    },
+
+    init: async function(){
         console.log( 'Video database init.' );
 
         this.initDB();
+
+        let n = await this.db.videodb.count();
+        $( '#information' ).text( `動画DBには${n}件あります` );
+
+        $( '#btn-add-db' ).on( 'click', ( ev ) =>{
+            this.addVideos();
+        } );
+        $( '#btn-search' ).on( 'click', ( ev ) =>{
+            this.searchVideos();
+        } );
 
         this.addCondLine();
         $( document ).on( 'click', '.btn-add-cond', ( ev ) =>{
